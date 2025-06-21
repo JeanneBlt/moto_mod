359699e42e3d9aa6367a9b5992b2a1cbb2bab3fb
unknown <jeanne.boulet@edu.esiee.fr>
2025-06-21 14:00:26 +0200

    Initial commit

---
+++ src/main/java/com/pyloufass/motomod/MotoModClient.java
@@ -0,0 +1,10 @@
+package com.pyloufass.motomod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MotoModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
